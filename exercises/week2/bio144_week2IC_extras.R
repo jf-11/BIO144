66608f24d3fe893f41b3257266d2db2eaa5ac2bb
jf-11 <63153161+jf-11@users.noreply.github.com>
2021-03-05 09:13:02 +0100

    Add files via upload

---
+++ exercises/week2/bio144_week2IC_extras.R
@@ -0,0 +1,21 @@
+########################################################################
+
+# BIO144
+# WEEK 2 IC
+
+########################################################################
+# EXTRAS
+########################################################################
+
+###### dplyr functions #####
+
+#select(): keep or exclude some columns
+#filter(): keep rows that satisfy your conditions
+#relocate(): move columns around
+#rename(): rename columns
+#mutate(): add a new column
+#across(): apply a function across columns
+#group_by() + summarize(): get summary statistics by group
+#count(): quickly find counts for different groups
+#case_when(): like friendly if-else
+
